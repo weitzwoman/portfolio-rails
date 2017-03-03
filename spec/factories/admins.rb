4c445353900a48203503f4e832931c7a48bd3d7a
Tracie Weitzman <tlweitzman@gmail.com>
2017-03-03 11:06:15 -0800

    add and remove validations, begin testing

--- spec/factories/admins.rb
+++ spec/factories/admins.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :admin do
-    
+    email "admin@email.com"
+    password "password"
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,11 +32,22 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.include Warden::Test::Helpers
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  # config.use_transactional_fixtures = true
+  #
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -57,4 +68,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
