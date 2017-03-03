89d7c5778171638b430cf55439ca163a923369ca
Tracie Weitzman <tlweitzman@gmail.com>
2017-03-03 14:07:48 -0800

    change cassette to feature a few more projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,7 +5,7 @@ class Project < ActiveRecord::Base
     def get_projects
       response = RestClient::Request.new(
       :method => :get,
-      :url => 'https://api.github.com/users/weitzwoman/repos?per_page=15',
+      :url => 'https://api.github.com/users/weitzwoman/repos?per_page=50',
       headers: {:access_token => ENV['API_KEY']}).execute
       response = JSON.parse(response)
     end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 describe Project do
   it 'will describe the get_projects model method', :vcr => true do
     projects = Project.get_projects
-    expect(projects.length).to eq(15)
+    expect(projects.length).to eq(50)
   end
 end
